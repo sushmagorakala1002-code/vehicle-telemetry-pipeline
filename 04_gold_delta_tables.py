d6e11212af689bc155dd6f3e9b0ecfc018000fb4
sushmagorakala1002-code <sushmagorakala1002@gmail.com>
2026-02-25 14:41:27 +0530

    04_gold_delta_tables.py

--- 04_gold_delta_tables.py
+++ 04_gold_delta_tables.py
@@ -4,27 +4,27 @@
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC DROP TABLE IF EXISTS default.vehicle_speed_5min;
-# MAGIC DROP TABLE IF EXISTS default.vehicle_engine_temp_5min;
-# MAGIC DROP TABLE IF EXISTS default.vehicle_event_count_5min;
+DROP TABLE IF EXISTS default.vehicle_speed_5min;
+DROP TABLE IF EXISTS default.vehicle_engine_temp_5min;
+DROP TABLE IF EXISTS default.vehicle_event_count_5min;
 
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC CREATE TABLE IF NOT EXISTS default.vehicle_speed_5min
-# MAGIC USING DELTA
-# MAGIC LOCATION 'abfss://gold@justpracticeadls.dfs.core.windows.net/vehicle_speed_5min';
+CREATE TABLE IF NOT EXISTS default.vehicle_speed_5min
+USING DELTA
+LOCATION 'abfss://gold@justpracticeadls.dfs.core.windows.net/vehicle_speed_5min';
 
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC CREATE TABLE IF NOT EXISTS default.vehicle_engine_temp_5min
-# MAGIC USING DELTA
-# MAGIC LOCATION 'abfss://gold@justpracticeadls.dfs.core.windows.net/vehicle_engine_temp_5min';
+CREATE TABLE IF NOT EXISTS default.vehicle_engine_temp_5min
+USING DELTA
+LOCATION 'abfss://gold@justpracticeadls.dfs.core.windows.net/vehicle_engine_temp_5min';
 
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC CREATE TABLE IF NOT EXISTS default.vehicle_event_count_5min
-# MAGIC USING DELTA
-# MAGIC LOCATION 'abfss://gold@justpracticeadls.dfs.core.windows.net/vehicle_event_count_5min';
+CREATE TABLE IF NOT EXISTS default.vehicle_event_count_5min
+USING DELTA
+LOCATION 'abfss://gold@justpracticeadls.dfs.core.windows.net/vehicle_event_count_5min';
